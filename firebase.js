ab5032b4a7808a4853040216fc3c00c540a34a93
munera-alzamil <alzamilmunera@gmail.com>
2025-05-23 09:31:05 +0300

    Update firebase.js
    تحديث الكود وربط الواجهة مع قاعدة البيانات

--- firebase.js
+++ firebase.js
@@ -1,12 +1,7 @@
-//npm install firebase
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; 
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBijALYt93Qs_4MfqwqRzE1O9XxdTBUPsg",
   authDomain: "cloud-computing-project-201ed.firebaseapp.com",
@@ -18,6 +13,8 @@ const firebaseConfig = {
   measurementId: "G-54JCXS5JPV"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app); //  هذا يربط Firestore
+
+export { db };
